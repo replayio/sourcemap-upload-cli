3d473a3c7e2be91677a275a74c785c3f66a3f249
Logan Smyth <loganfsmyth@gmail.com>
2021-04-27 10:44:59 -0700

    Initial CLI implementation.

---
+++ bin/sourcemap-upload.js
@@ -0,0 +1,3 @@
+/* Copyright 2020 Record Replay Inc. */
+
+require("../lib/bin");
---
+++ src/bin.ts
@@ -0,0 +1,83 @@
+/* Copyright 2020 Record Replay Inc. */
+
+import { program } from "commander";
+import { uploadSourceMaps, MessageLevel } from "@recordreplay/sourcemap-upload";
+
+program
+  .requiredOption(
+    "-g, --group <name>",
+    "The name to group this sourcemap into, e.g. A commit SHA or release version."
+  )
+  .option(
+    "-k, --key <key>",
+    "The Replay API key to use. Defaults to the value of 'process.env.RECORD_REPLAY_API_KEY'."
+  )
+  .option(
+    "--dry-run",
+    "Perform all of the usual CLI logic, but the final sourcemap upload."
+  )
+  .option(
+    "-x, --extensions <exts>",
+    "A comma-separated list of extensions to process. Defaults to '.js,.map'.",
+    collectExtensions
+  )
+  .option(
+    "-i, --ignore <pattern>",
+    "Ignore files that match this pattern",
+    collectIgnorePatterns
+  )
+  .option("-q, --quiet", "Silence all stdout logging.")
+  .option("-v, --verbose", "Output extra data to stdout when processing files.")
+  .option(
+    "--root <dirname>",
+    "The base directory to use when computing relative paths"
+  )
+  .arguments("<paths...>")
+  .action((filepaths, opts) => sourcemapUpload(filepaths, opts))
+  .parseAsync();
+
+function collectExtensions(value: string) {
+  return value.split(",");
+}
+function collectIgnorePatterns(value: string, previous: Array<string> = []) {
+  return previous.concat([value]);
+}
+
+interface CLIOptions {
+  group: string;
+  key?: string;
+  dryRun?: boolean;
+  extensions?: Array<string>;
+  ignore?: Array<string>;
+  quiet?: boolean;
+  verbose?: boolean;
+  root?: string;
+}
+
+async function sourcemapUpload(
+  filepaths: Array<string>,
+  cliOpts: CLIOptions
+): Promise<void> {
+  const { quiet, verbose, ...uploadOpts } = cliOpts;
+
+  let log;
+  if (!quiet) {
+    if (verbose) {
+      log = (level: MessageLevel, message: string) => {
+        console.log(message);
+      };
+    } else {
+      log = (level: MessageLevel, message: string) => {
+        if (level === MessageLevel.Normal) {
+          console.log(message);
+        }
+      };
+    }
+  }
+
+  await uploadSourceMaps({
+    filepaths,
+    ...uploadOpts,
+    log,
+  });
+}
